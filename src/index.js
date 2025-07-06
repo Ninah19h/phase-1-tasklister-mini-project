369ae719f97bd25fa482d541deee0dd01c68795b
Ninah19h <jerolinaha@gmail.com>
2025-07-06 23:27:27 +0300

    Code challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  const input = document.getElementById("new-task-description");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent the form from s refreshing the page
+
+    const task = input.value.trim();
+
+    if (task !== "") {  //
+      const li = document.createElement("li"); 
+      li.textContent = task;
+
+      taskList.appendChild(li)
+      input.value = "";
+       }
+  });
 });
